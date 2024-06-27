5c340d47867c7a5e259c15eb3a102e13d3b17a6c
jigneshbhandi09 <95614233+jigneshbhandi09@users.noreply.github.com>
2024-06-27 10:19:41 +0530

    2_array.py

---
+++ 2_array.py
@@ -0,0 +1,9 @@
+numbers = []
+
+num_elements = int(input("How many numbers do you want to add to the array? "))
+
+for _ in range(num_elements):
+    new_number = int(input("Enter a number: "))
+    numbers.append(new_number)
+
+print("Final array:", numbers)
